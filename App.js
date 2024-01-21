dcbdf759203d18de3d74f00231c5c2a459c4108d
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-21 12:36:48 +0200

    Added Home Screen

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import HomeScreen from "./screens/HomeScreen";
+import IconButton from "./components/UI/IconButton";
+import { COLORS } from "./constants/COLORS";
+
+const Bottom = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>The MoneyFlow App!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Bottom.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: COLORS.peach },
+            headerTintColor: "white",
+            tabBarStyle: { backgroundColor: COLORS.peach },
+            tabBarActiveTintColor: "white",
+          }}
+        >
+          <Bottom.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              title: "Home",
+              // tabBarLabel: "Home Screen",
+              tabBarIcon: ({ color, size }) => (
+                <IconButton icon="home" color={color} size={size} />
+              ),
+            }}
+          />
+        </Bottom.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
---
+++ constants/colors.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+  // base colors
+  primary: "#194868", // Dark Blue
+  secondary: "#FF615F", // peach
+
+  // colors
+  black: "#1E1F20",
+  white: "#FFFFFF",
+  lightGray: "#F5F7F9",
+  lightGray2: "#FAFBFD",
+  gray: "#BEC1D2",
+  blue: "#42B0FF",
+  darkgray: "#898C95",
+  yellow: "#FFD573",
+  lightBlue: "#95A9B8",
+  darkgreen: "#008159",
+  peach: "#FF615F",
+  purple: "#8e44ad",
+  red: "#FF0000",
+};
