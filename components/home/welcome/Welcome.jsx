53802de484db6cac5478dc32cbc3bd67592ce5b3
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-01-21 09:23:44 +0200

    Begin from Scratch

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 
 const Welcome = () => {
+  const router = useState();
+
   return (
     <View>
       <Text>Welcome</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
