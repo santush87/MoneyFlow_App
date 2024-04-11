d1888e1770f216857cf9659387f40fcb27fde137
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-11 11:36:24 +0300

    Added ModelMapper and created a bean in AppConfig

---
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.martin.aleksandrov.backend.config;
+
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        final ModelMapper mapper = new ModelMapper();
+        mapper.addConverter(new Converter<String, LocalDate>() {
+            @Override
+            public LocalDate convert(MappingContext<String, LocalDate> context) {
+                return LocalDate.parse(context.getSource(),
+                        DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+        });
+        return mapper;
+    }
+}
