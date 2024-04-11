562b481bc0a3f49266a36cfa24e953c9a0119f95
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-11 11:38:49 +0300

    Removed unused import

--- backend/src/main/java/com/martin/aleksandrov/backend/config/AppConfig.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/AppConfig.java
@@ -2,7 +2,6 @@ package com.martin.aleksandrov.backend.config;
 
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
