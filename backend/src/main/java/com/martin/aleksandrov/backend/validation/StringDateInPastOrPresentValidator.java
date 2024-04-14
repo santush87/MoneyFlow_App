aa85b4cc6de79038cd04570fd2b8fcfa79f11362
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-14 12:10:17 +0300

    Created custom annotation to check if the given date is current or in the past

---
+++ backend/src/main/java/com/martin/aleksandrov/backend/validation/StringDateInPastOrPresentValidator.java
@@ -0,0 +1,23 @@
+package com.martin.aleksandrov.backend.validation;
+
+import com.martin.aleksandrov.backend.annotation.StringDateInPastOrPresent;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class StringDateInPastOrPresentValidator implements ConstraintValidator<StringDateInPastOrPresent, String> {
+    @Override
+    public void initialize(StringDateInPastOrPresent constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (value != null && !value.isBlank()) {
+            LocalDate parse = LocalDate.parse(value);
+            return parse.isBefore(LocalDate.now().plusDays(1));
+        }
+        return false;
+    }
+}
