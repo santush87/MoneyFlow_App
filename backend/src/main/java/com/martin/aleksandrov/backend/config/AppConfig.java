8d84928f2a65e8bdd122d60c03acffcc9a42f8da
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-14 17:29:17 +0300

    Fix problem with modelmapper for local date mapping

--- backend/src/main/java/com/martin/aleksandrov/backend/config/AppConfig.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.martin.aleksandrov.backend.config;
 
-import org.modelmapper.Converter;
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +14,14 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         final ModelMapper mapper = new ModelMapper();
-        mapper.addConverter((Converter<String, LocalDate>) context -> LocalDate.parse(context.getSource(),
-                DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+        // Add mapping from String to LocalDate
+        mapper.addConverter(new AbstractConverter<String, LocalDate>() {
+            @Override
+            protected LocalDate convert(String source) {
+                return LocalDate.parse(source, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            }
+        });
         return mapper;
     }
 }
