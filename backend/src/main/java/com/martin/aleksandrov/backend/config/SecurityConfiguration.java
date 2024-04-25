f438c21feddc34b0226eda0b1c9652f5fc482733
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-25 12:54:41 +0300

    Added filters

--- backend/src/main/java/com/martin/aleksandrov/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/SecurityConfiguration.java
@@ -1,12 +1,38 @@
 package com.martin.aleksandrov.backend.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll)
+                .csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
