bf8885aaf7e51bf82d0ed57dda93013355d39668
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-25 12:55:44 +0300

    Added implementation of UserDetails

--- backend/src/main/java/com/martin/aleksandrov/backend/models/entities/UserEntity.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/models/entities/UserEntity.java
@@ -7,16 +7,21 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class UserEntity{
+public class UserEntity implements UserDetails {
 
     @Id
     @UuidGenerator
@@ -54,5 +59,37 @@ public class UserEntity{
     public UserEntity() {
         this.createdOn = LocalDate.now();
         this.categories = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
