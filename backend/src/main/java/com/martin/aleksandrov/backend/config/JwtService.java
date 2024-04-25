2ea79ceeb8bf0da7688bea12b8a1e88b863e78e5
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-04-25 11:59:02 +0300

    Added isValidToken method

--- backend/src/main/java/com/martin/aleksandrov/backend/config/JwtService.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/JwtService.java
@@ -43,9 +43,22 @@ public class JwtService {
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(
                         System.currentTimeMillis()
-                        + 1000 * 60 * 24))
+                        + 1000 * 60 * 24 * 14))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
