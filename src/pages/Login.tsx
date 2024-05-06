c3f6799d7724e1c06fe93178ed3622a8b473cc8c
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-05-06 15:14:56 +0300

    Code refactoring

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,7 +23,7 @@ export default function Login() {
 
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className='max-w-lg mr-auto ml-auto px-16 py-4 shadow-2xl bg-slate-50'>
+            <div className='max-w-lg mr-auto ml-auto px-16 py-4 shadow-2xl rounded-2xl bg-slate-50'>
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         className="mx-auto h-10 w-auto"
@@ -36,9 +36,13 @@ export default function Login() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6" action="#" method="POST">
+                    <form onSubmit={handleSubmit(onSubmit)}
+                        className="space-y-6"
+                        action="#"
+                        method="POST">
                         <div>
-                            <label htmlFor="email" className="block text-md font-medium leading-6 text-gray-900">
+                            <label htmlFor="email"
+                                className="block text-md font-medium leading-6 text-gray-900">
                                 Имейл
                             </label>
                             <div className="mt-2">
@@ -47,14 +51,15 @@ export default function Login() {
                                     type="email"
                                     placeholder='Email'
                                     autoComplete="email"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
 
                         <div>
                             <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-md font-medium leading-6 text-gray-900">
+                                <label htmlFor="password"
+                                    className="block text-md font-medium leading-6 text-gray-900">
                                     Парола
                                 </label>
                                 <div className="text-sm">
@@ -67,8 +72,8 @@ export default function Login() {
                                 <input
                                     {...register("password")}
                                     type="password"
-                                    autoComplete="current-password"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    placeholder='Password'
+                                    className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                                 {errors.password && (
                                     <p className='text-red-500'>{`${errors.password.message}`}</p>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -24,7 +24,7 @@ export default function Register() {
 	}
 	return (
 		<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-			<div className='max-w-lg mr-auto ml-auto px-16 py-4 shadow-2xl bg-slate-50'>
+			<div className='max-w-lg mr-auto ml-auto px-16 py-4 shadow-2xl rounded-2xl bg-slate-50'>
 				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
 					<img
 						className="mx-auto h-10 w-auto"
@@ -36,10 +36,15 @@ export default function Register() {
 					</h2>
 				</div>
 				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-					<form onSubmit={handleSubmit(onSubmit)} className="space-y-6" action="#" method="POST">
+					{/* FORM */}
+					<form onSubmit={handleSubmit(onSubmit)}
+						className="space-y-6"
+						action="#"
+						method="POST">
 						{/* EMAIL */}
 						<div>
-							<label htmlFor="email" className="block text-md font-medium leading-6 text-gray-900">
+							<label htmlFor="email"
+								className="block text-md font-medium leading-6 text-gray-900">
 								Имейл
 							</label>
 							<div className="mt-2">
@@ -78,7 +83,9 @@ export default function Register() {
 							</div>
 							{/* LAST NAME */}
 							<div className='flex flex-col'>
-								<label htmlFor="firstName" className="block text-md font-medium leading-6 text-gray-900">
+								<label htmlFor="firstName"
+									className="block text-md font-medium 
+								leading-6 text-gray-900">
 									Фамилия
 								</label>
 								<div className="mt-2">
@@ -93,51 +100,57 @@ export default function Register() {
 									sm:text-sm sm:leading-6 px-2"
 									/>
 								</div>
-
 							</div>
 						</div>
 						<div>
 							{/* PASSWORD */}
-							<div className="flex items-center justify-between">
-								<label htmlFor="password" className="block text-md font-medium leading-6 text-gray-900">
-									Парола
-								</label>
+							<div className='flex gap-2'>
+								<div className="flex flex-col">
+									<label htmlFor="password"
+										className="block text-md font-medium
+										 leading-6 text-gray-900">
+										Парола
+									</label>
+									<div className="mt-2">
+										<input
+											{...register("password")}
+											type="password"
+											placeholder='Password'
+											className="block w-full rounded-md border-0 py-1.5
+									text-gray-900 shadow-sm ring-1 ring-inset
+									ring-gray-300 placeholder:text-gray-400 
+									focus:ring-2 focus:ring-inset focus:ring-indigo-600 
+									sm:text-sm sm:leading-6 px-2"
+										/>
+										{errors.password && (
+											<p className='text-red-500'>{`${errors.password.message}`}</p>
+										)}
+									</div>
+								</div>
+								{/*CONFIRM PASSWORD */}
+								<div className='flex flex-col'>
+									<label htmlFor="confirmPassword"
+										className="block text-md 
+										font-medium leading-6 text-gray-900">
+										Пoтвърди парола
+									</label>
 
-							</div>
-							<div className="mt-2">
-								<input
-									{...register("password")}
-									type="password"
-									autoComplete="current-password"
-									className="block w-full rounded-md border-0 py-1.5
-									 text-gray-900 shadow-sm ring-1 ring-inset
-									  ring-gray-300 placeholder:text-gray-400 focus:ring-2 
-									  focus:ring-inset focus:ring-indigo-600 sm:text-sm 
-									  sm:leading-6"
-								/>
-								{errors.password && (
-									<p className='text-red-500'>{`${errors.password.message}`}</p>
-								)}
-							</div>
-							{/*CONFIRM PASSWORD */}
-							<div className="flex items-center justify-between">
-								<label htmlFor="confirmPassword" className="block text-md font-medium leading-6 text-gray-900">
-									Пoтвърди парола
-								</label>
-
-							</div>
-							<div className="mt-2">
-								<input
-									{...register("confirmPassword")}
-									type="password"
-									className="block w-full rounded-md border-0 py-1.5
-									 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
-									  placeholder:text-gray-400 focus:ring-2 focus:ring-inset
-									   focus:ring-indigo-600 sm:text-sm sm:leading-6"
-								/>
-								{errors.password && (
-									<p className='text-red-500'>{`${errors.confirmPassword.message}`}</p>
-								)}
+									<div className="mt-2">
+										<input
+											{...register("confirmPassword")}
+											type="password"
+											placeholder='Confirm password'
+											className="block w-full rounded-md border-0 py-1.5
+									text-gray-900 shadow-sm ring-1 ring-inset
+									ring-gray-300 placeholder:text-gray-400 
+									focus:ring-2 focus:ring-inset focus:ring-indigo-600 
+									sm:text-sm sm:leading-6 px-2"
+										/>
+										{errors.confirmPassword && (
+											<p className='text-red-500'>{`${errors.confirmPassword.message}`}</p>
+										)}
+									</div>
+								</div>
 							</div>
 						</div>
 
@@ -145,22 +158,26 @@ export default function Register() {
 							<button
 								type="submit"
 								disabled={isSubmitting}
-								className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+								className="flex w-full justify-center rounded-md bg-indigo-600
+								 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+								 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 
+								 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 							>
-								Влез
+								Изпрати
 							</button>
 						</div>
 					</form>
 
 					<p className="mt-10 text-center text-sm text-gray-500">
-						Нямаш профил?{' '}
-						<Link to="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-							Регистрирай се тук!
+						Имаш профил?{' '}
+						<Link to="/login" className="font-semibold leading-6 text-indigo-600 
+						hover:text-indigo-500">
+							Впиши се от тук!
 						</Link>
 					</p>
 				</div>
-			</div>
-		</div>
+			</div >
+		</div >
 	)
 }
 
