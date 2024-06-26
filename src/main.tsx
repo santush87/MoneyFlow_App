2644c15c5fea36978680afd8b3c9518e0cbf2670
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-06-26 20:26:43 +0300

    Added QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,17 @@ import './styles/reset.css'
 import './styles/typography.css'
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+const queryClient = new QueryClient({})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
