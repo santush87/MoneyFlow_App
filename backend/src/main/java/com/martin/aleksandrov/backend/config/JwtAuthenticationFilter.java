dc838d54fdabd2bf0075f080cce72e342acb79f4
Martin Aleksandrov <martin.aleksandrov87@gmail.com>
2024-06-27 08:51:09 +0300

    Changed path

--- backend/src/main/java/com/martin/aleksandrov/backend/config/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/martin/aleksandrov/backend/config/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (request.getServletPath().contains("/api/v1/auth")) {
+        if (request.getServletPath().contains("/user/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
